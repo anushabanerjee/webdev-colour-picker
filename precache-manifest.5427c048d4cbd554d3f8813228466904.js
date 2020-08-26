0a64f44f056ae647ad5d5f534e48ac06eadb6417
Anusha Banerjee <rairaianusha@gmail.com>
2020-08-26 21:19:27 +0530

    Updates

---
+++ precache-manifest.5427c048d4cbd554d3f8813228466904.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1cc3047527a8e43d7c9d57d8d70101a",
+    "url": "/webdev-colour-picker/index.html"
+  },
+  {
+    "revision": "718ca18c5e0ee758b157",
+    "url": "/webdev-colour-picker/static/css/main.6ca4c94c.chunk.css"
+  },
+  {
+    "revision": "ecf7cbaaa1f28fd9913a",
+    "url": "/webdev-colour-picker/static/js/2.b08efecd.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/webdev-colour-picker/static/js/2.b08efecd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "718ca18c5e0ee758b157",
+    "url": "/webdev-colour-picker/static/js/main.c4ebbc1f.chunk.js"
+  },
+  {
+    "revision": "0c01fb320f3cc24c8cb5",
+    "url": "/webdev-colour-picker/static/js/runtime-main.db394dcd.js"
+  }
+]);
